2fcd172c8bb56d5d7425c95c74f18295dd176921
Stephen Thirlwall <sdt@dr.com>
2012-04-06 16:23:39 +1000

    Move option handling and pod2usage back to script

--- lib/App/USelect.pm
+++ lib/App/USelect.pm
@@ -7,9 +7,12 @@ use Modern::Perl;
 use App::USelect::Selector;
 use App::USelect::UI::Curses;
 use Getopt::Long qw( GetOptionsFromArray );
-use Pod::Usage qw( pod2usage );
 use Try::Tiny;
 use autodie;
+
+use parent 'Exporter';
+
+our @EXPORT_OK = qw( run );
 
 # VERSION
 
@@ -18,18 +21,12 @@ use autodie;
 #open(STDERR, '>', '/tmp/uselect.log');
 
 sub run {
-    my ($class, @ARGV) = @_;
+    my ($opt, $argv) = @_;
 
-    my %opt = (
-        include_blanks  => 0,
-        select_code     => '1',
-    );
+    my $select_sub = _make_select_sub($opt)
+        or return 4;
 
-    _process_options(\%opt, \@ARGV);
-    my $select_sub = _make_select_sub(\%opt);
-
-    chomp(my @lines = @ARGV ? @ARGV : <STDIN>);
-
+    chomp(my @lines = @$argv ? @$argv : <STDIN>);
     my $selector = App::USelect::Selector->new(
             text          => \@lines,
             is_selectable => $select_sub,
@@ -37,7 +34,7 @@ sub run {
 
     if ($selector->selectable_lines == 0) {
         say STDERR 'No selectable lines.';
-        exit 2;
+        return 2;
     }
 
     my $ui = App::USelect::UI::Curses->new(
@@ -47,28 +44,13 @@ sub run {
 
     if ($ui->has_errors) {
         say STDERR 'ERROR:', $ui->errors;
-        exit 3;
+        return 3;
     }
     say $_->text for $selector->selected_lines;
-    exit ($selector->selected_lines == 0);
+    return ($selector->selected_lines == 0);
 }
 
 #-------------------------------------------------------------------------------
-
-sub _process_options {
-    my ($opt, $argv) = @_;
-    my @options_spec = (
-        "blank_lines|b"     => \$opt->{include_blanks},
-        "help|h|?"          => sub { pod2usage(0) },
-        "select|s=s"        => \$opt->{select_code},
-        "version|v"         => sub {
-            say "uselect v$App::USelect::VERSION"; exit 0 },
-    );
-
-    if (not GetOptionsFromArray($argv, @options_spec)) {
-        pod2usage(1);
-    }
-}
 
 sub _make_select_sub {
     my ($opt) = @_;
@@ -91,7 +73,10 @@ sub _make_select_sub {
     }
 
     my $select_sub = eval('sub { $_ = shift; ' . $opt->{select_code} . '}'); ## no critic ProhibitStringyEval
-    pod2usage($@) if $@;    # this may still occur even if the first parse succeeded
+    if ($@) {
+        say STDERR $@;
+        return;
+    }
 
     return $select_sub;
 }
@@ -101,53 +86,9 @@ __END__
 
 =pod
 
-=head1 SYNOPSIS
+=head1 FUNCTIONS
 
-uselect [options] [select text]
-
-  options:
-     --blank_lines, -b
-        Force blank lines to be selectable.
-
-    --help, -h, -?
-        Show this help.
-
-    --select <perl expression>, -s <perl expression>
-        Select only those lines for which given perl expression evaluates to
-        true. The input lines are passed as $_.
-
-    --version, -v
-        Print the version and exit.
-
-  Input lines can be specified on the command line after the options, otherwise
-  they will be read from stdin.
-
-=head1 DESCRIPTION
-
-uselect is a reimplementation of iselect by Ralf S. Engelschall
-
-    http://www.ossp.org/pkg/tool/iselect/
-
-uselect is intended to be used as an interactive unix filter. Input lines are
-displayed to the user, and selected lines are written to stderr.
-
-=head1 SERVING SUGGESTIONS
-
-fv() { vim $( find . -type f | sort | fgrep "$@" | uselect ); }
-
-gv() { vim $( ack --heading --break "$@" | uselect -s '!/^\d+:/ ); }
-
-=cut
-
-1;
-
-__END__
-
-=pod
-
-=head1 METHODS
-
-=head2 run( @ARGV )
+=head2 run( \%options, \@ARGV )
 
 Runs the application
 
